c4d1de7ee95ee0372edd156e7378b5b19ad9c834
Arturo Reyes <arturo.reyes@epicor.com>
2023-06-20 16:33:40 -0600

    Creating components and basic examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todos';
+  nombre: string  = "Arturo Reyex";
+
+  cargando : boolean = false
+
+  color : string = 'red';
+
+  cambios : number = 0;
+
+  tareas: string[] = [];
+  //tareas: Array<string> = [];
+
+
+
+  constructor() {
+
+    this.cargando = true;
+
+    setTimeout(() => {
+      this.nombre   = "Pedro";
+      this.cargando = false;      
+      this.tareas   = ['tarea1','tarea2','tarea3'];
+    },3000);
+  }
+
+  cambiarColor() {
+    this.color = this.color === 'red' ? 'blue' : 'red';
+    this.cambios++;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './pages/todos/todos.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
